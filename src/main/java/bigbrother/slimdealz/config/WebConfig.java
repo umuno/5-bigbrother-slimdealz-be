3f788b1ce6e84a285c89122885259393fc091c75
rhea <dev.rhea4444@gmail.com>
2024-08-11 20:32:09 +0900

    :sparkles: Config

---
+++ src/main/java/bigbrother/slimdealz/config/WebConfig.java
@@ -0,0 +1,27 @@
+package bigbrother.slimdealz.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Value("${ClientURL")
+    private String clientURL;
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowCredentials(true)
+                        .allowedOrigins(clientURL)
+                        .allowedMethods("GET", "POST", "PUT", "DELETE");
+            }
+
+        };
+    }
+}
