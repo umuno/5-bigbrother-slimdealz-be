bafe2f01445af570baa1bf68f07f7b9eefd9dbe4
rhea <dev.rhea4444@gmail.com>
2024-08-11 20:32:29 +0900

    :sparkles: Exception settings

---
+++ src/main/java/bigbrother/slimdealz/exception/CustomExceptionHandler.java
@@ -0,0 +1,21 @@
+package bigbrother.slimdealz.exception;
+
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Slf4j
+@RestControllerAdvice
+public class CustomExceptionHandler {
+    @ExceptionHandler(CustomException.class)
+    public CustomErrorResponse handleCustomException(CustomException e, HttpServletRequest request) {
+        log.error("error code : {}, url {}, message: {}",
+                e.getCustomErrorCode(), request.getRequestURI(), e.getDetalMessage());
+
+        return CustomErrorResponse.builder()
+                .code(e.getCustomErrorCode())
+                .message(e.getDetalMessage())
+                .build();
+    }
+}
